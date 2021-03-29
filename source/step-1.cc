1ae8e0f67f139f65b7f419e133c21a5ad2da263b
Ivan Prusak <iv.prusak@gmail.com>
2021-03-29 18:31:59 +0200

    steps 1-6 og the assignment

--- source/step-1.cc
+++ source/step-1.cc
@@ -36,12 +36,27 @@ first_grid()
   Triangulation<2> triangulation;
 
   GridGenerator::hyper_cube(triangulation);
+  std::cout << "Number of original vertices:" << triangulation.n_vertices()
+            << std::endl;
+
   triangulation.refine_global(4);
 
-  std::ofstream out("grid-1.svg");
-  GridOut       grid_out;
-  grid_out.write_svg(triangulation, out);
-  std::cout << "Grid written to grid-1.svg" << std::endl;
+  std::cout << "Number of original vertices after 4 refinements:"
+            << triangulation.n_vertices() << std::endl;
+
+  {
+    std::ofstream out("grid-1.svg");
+    GridOut       grid_out;
+    grid_out.write_svg(triangulation, out);
+    std::cout << "Grid written to grid-1.svg" << std::endl;
+  }
+
+  {
+    std::ofstream out("grid-1.vtk");
+    GridOut       grid_out;
+    grid_out.write_vtk(triangulation, out);
+    std::cout << "Grid written to grid-1.vtk" << std::endl;
+  }
 }
 
 
@@ -55,8 +70,14 @@ second_grid()
   const double   inner_radius = 0.5, outer_radius = 1.0;
   GridGenerator::hyper_shell(
     triangulation, center, inner_radius, outer_radius, 10);
+
+  // triangulation.reset_all_manifolds();
   for (unsigned int step = 0; step < 5; ++step)
     {
+      std::ofstream out("grid-2-" + std::to_string(step) + ".vtk");
+      GridOut       grid_out;
+      grid_out.write_vtk(triangulation, out);
+
       for (auto &cell : triangulation.active_cell_iterators())
         {
           for (const auto v : cell->vertex_indices())
@@ -75,13 +96,53 @@ second_grid()
 
       triangulation.execute_coarsening_and_refinement();
     }
+}
+
+void
+third_grid()
+{
+  Triangulation<2> triangulation;
+  GridGenerator::hyper_L(triangulation);
+  std::cout << "Number of original vertices:" << triangulation.n_vertices()
+            << std::endl;
+
+  triangulation.refine_global(1);
+
+  std::cout << "Number of original vertices after 1 refinement:"
+            << triangulation.n_vertices() << std::endl;
 
 
-  std::ofstream out("grid-2.svg");
-  GridOut       grid_out;
-  grid_out.write_svg(triangulation, out);
+  {
+    std::ofstream out("grid-3.vtk");
+    GridOut       grid_out;
+    grid_out.write_vtk(triangulation, out);
+    std::cout << "Grid written to grid-3.vtk" << std::endl;
+  }
 
-  std::cout << "Grid written to grid-2.svg" << std::endl;
+  const Point<2> corner(0, 0);
+  for (unsigned int step = 0; step < 5; ++step)
+    {
+      std::ofstream out("grid-3-" + std::to_string(step) + ".vtk");
+      GridOut       grid_out;
+      grid_out.write_vtk(triangulation, out);
+
+      for (auto &cell : triangulation.active_cell_iterators())
+        {
+          for (const auto v : cell->vertex_indices())
+            {
+              const double distance_from_center =
+                corner.distance(cell->vertex(v));
+
+              if (std::fabs(distance_from_center) <= 1. / 3.)
+                {
+                  cell->set_refine_flag();
+                  break;
+                }
+            }
+        }
+
+      triangulation.execute_coarsening_and_refinement();
+    }
 }
 
 
@@ -91,4 +152,5 @@ main()
 {
   first_grid();
   second_grid();
+  third_grid();
 }
